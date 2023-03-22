25dbac5ed04ee0414190ddd42623efc14a015022
aglitus <joaolucas123.ba@gmail.com>
2023-03-21 23:43:27 -0300

    Primeiro commit

---
+++ app/Http/Controllers/FecharChamadoController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FecharChamado;
+use App\Http\Requests\StoreFecharChamadoRequest;
+use App\Http\Requests\UpdateFecharChamadoRequest;
+
+class FecharChamadoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFecharChamadoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FecharChamado $fecharChamado)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FecharChamado $fecharChamado)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateFecharChamadoRequest $request, FecharChamado $fecharChamado)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FecharChamado $fecharChamado)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/UltChamadosController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UltChamados;
+use App\Http\Requests\StoreUltChamadosRequest;
+use App\Http\Requests\UpdateUltChamadosRequest;
+
+class UltChamadosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUltChamadosRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UltChamados $ultChamados)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UltChamados $ultChamados)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUltChamadosRequest $request, UltChamados $ultChamados)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UltChamados $ultChamados)
+    {
+        //
+    }
+}
