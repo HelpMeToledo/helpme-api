d514a0e91c01b490fe5c160cf24a2cb1ad092225
feliperagne <frsilveira01@outlook.com>
2023-04-12 19:27:53 -0300

    melhorando a validação do formulario

--- app/Http/Requests/StoreUsuariosRequest.php
+++ app/Http/Requests/StoreUsuariosRequest.php
@@ -21,12 +21,25 @@ class StoreUsuariosRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [             
-            'cpf' => 'required|max:11',
-            'email' => 'required',
-            'senha' => 'required',
-            'telefone' => 'required|max:9',
-
+        return [   
+            'nome' => 'required',          
+            'cpf' => 'required|size:11',
+            'email' => 'required|email|unique',
+            'senha' => 'required|min:3',
+            'telefone' => 'required|size:11',
         ];
     }
+    public function messages(): array
+    {
+    return [
+        'nome.required' => 'O campo nome é obrigatório.',
+        'email.required' => 'O campo e-mail é obrigatório.',
+        'email.email' => 'Por favor, informe um endereço de e-mail válido.',
+        'email.unique' => 'O endereço de e-mail informado já está sendo utilizado por outra pessoa!',
+        'senha.required' => 'O campo senha é obrigatório.',
+        'senha.min' => 'A senha deve conter pelo menos 3 caracteres.',
+        'telefone.required' => 'O campo de telefone é obrigatório.',
+        'telefone.size' =>'O telefone deve ter 11 números. Use seu DDD!'
+    ];
+  }
 }
--- app/Http/Requests/UpdateUsuariosRequest.php
+++ app/Http/Requests/UpdateUsuariosRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateUsuariosRequest extends FormRequest
+class StoreUsuariosRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -21,11 +21,25 @@ class UpdateUsuariosRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            'cpf' => 'required|max:11',
-            'email' => 'required',
-            'senha' => 'required',
-            'telefone' => 'required|max:9',
+        return [   
+            'nome' => 'required',          
+            'cpf' => 'required|size:11',
+            'email' => 'required|email|unique: users.email'.$this->id(),
+            'senha' => 'required|min:3',
+            'telefone' => 'required|size:11',
         ];
     }
+    public function messages(): array
+    {
+    return [
+        'nome.required' => 'O campo nome é obrigatório.',
+        'email.required' => 'O campo e-mail é obrigatório.',
+        'email.email' => 'Por favor, informe um endereço de e-mail válido.',
+        'email.unique' => 'O endereço de e-mail informado já está sendo utilizado por outra pessoa!',
+        'senha.required' => 'O campo senha é obrigatório.',
+        'senha.min' => 'A senha deve conter pelo menos 3 caracteres.',
+        'telefone.required' => 'O campo de telefone é obrigatório.',
+        'telefone.size' =>'O telefone deve ter 11 números. Use seu DDD!'
+    ];
+  }
 }
